779f6ae15e61abcef128f896f611065ca70937ad
Pedro Lobo <pedro@arch.com>
2021-10-19 12:32:52 +0100

    Add some more methods

---
+++ src/ggc/core/Notification.java
@@ -0,0 +1,22 @@
+package ggc.core;
+
+public class Notification {
+	private String _type;
+	private Product _product;
+
+	public Notification(String type) {
+		_type = type;
+	}
+
+	public String getType() {
+		return _type;
+	}
+
+	public Product getProduct() {
+		return _product.copy();
+	}
+
+	public String toString() {
+		return _type + "|" + _product.getId() + "|" + _product.getLowestPrice();
+	}
+}
--- src/ggc/core/Warehouse.java
+++ src/ggc/core/Warehouse.java
@@ -2,6 +2,9 @@ package ggc.core;
 
 // FIXME import classes (cannot import from pt.tecnico or ggc.app)
 
+import java.util.Set;
+import java.util.HashSet;
+
 import java.io.Serializable;
 import java.io.IOException;
 import ggc.core.exception.BadEntryException;
@@ -11,20 +14,58 @@ import ggc.core.exception.BadEntryException;
  */
 public class Warehouse implements Serializable {
 
-  /** Serial number for serialization. */
-  private static final long serialVersionUID = 202109192006L;
+	/** Serial number for serialization. */
+	private static final long serialVersionUID = 202109192006L;
 
-  // FIXME define attributes
-  // FIXME define contructor(s)
-  // FIXME define methods
+	private double _accountingBalance;
+	private double _availableBalance;
+	private Date _date;
+	private Set<Product> _products;
+	private Set<Transaction> _transactions;
+	private Set<Partner> _partners;
 
-  /**
-   * @param txtfile filename to be loaded.
-   * @throws IOException
-   * @throws BadEntryException
-   */
-  void importFile(String txtfile) throws IOException, BadEntryException /* FIXME maybe other exceptions */ {
-    //FIXME implement method
-  }
+	public Warehouse() {
+		_accountingBalance = 0;
+		_availableBalance = 0;
+		_date = new Date();
+		_products = new HashSet<>();
+		_transactions = new HashSet<>();
+		_partners = new HashSet<>();
+	}
 
+	public void addProduct(Product product) {
+		_products.add(product);
+	}
+
+	public void removeProduct(Product product) {
+		_products.remove(product);
+	}
+
+	public void addPartner(Partner partner) {
+		_partners.add(partner);
+	}
+
+	public void addTransaction(Transaction transaction) {
+		_transactions.add(transaction);
+	}
+
+	public boolean hasStock(Product product) {
+		return product.hasStock();
+	}
+
+	public boolean aggregate(AggregateProduct product) {}
+
+	public void fowardTime(int increment) {
+		_date.add(increment);
+	}
+
+
+	/**
+	 * @param txtfile filename to be loaded.
+	 * @throws IOException
+	 * @throws BadEntryException
+	 */
+	void importFile(String txtfile) throws IOException, BadEntryException /* FIXME maybe other exceptions */ {
+		//FIXME implement method
+	}
 }
